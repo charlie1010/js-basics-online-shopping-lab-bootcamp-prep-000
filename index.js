c3d19f89e0fad1371728cd8e9c185052708c653c
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-14 20:21:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
       if (i===cart.length-2) {
          sentence += "and "
       }
-      if (i===cart.length-1) {
+      if (i===cart.length) {
          sentence
       }
     }
