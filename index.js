f45e3a207e515d710b20315dd1c94838c12ea7b9
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-14 19:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
     var sentence = "In your cart, you have "
     for (var i = 0, l = cart.length; i <l; i++) {
       //sentence += `${cart[]} at $${
-      
+      debugger;
       if (cart.length === 1) {
         
       } else {
