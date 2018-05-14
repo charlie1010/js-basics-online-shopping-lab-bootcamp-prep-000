338bc9bb9f2da1b248d29e64f6f304b1a6cd0c88
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-14 17:01:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var price = Math.floor(Math.random()*100)
   cart.push(Object.assign({itemName:item, itemPrice:price}))
-  console.log(`${item} has been added to your cart.`)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
