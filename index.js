7583cd6d6a3e2148da7f359feb3a0fe8c10b259e
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-14 19:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
       sentence += `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`
       if (cart.length > 1) {
         sentence += ", "
-       if (i===cart.length-2) {
+       if (i===cart.length-1) {
          sentence += ", and "
         }
       }
